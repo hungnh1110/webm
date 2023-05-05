5f70f83f4364f210b2bc418d24b7f82d915d4ee6
hungnh1110 <hungnh.fw@gmail.com>
2023-05-05 13:19:51 +0700

    upload

---
+++ footer.php
@@ -0,0 +1,84 @@
+<?php
+
+?>
+<!--::footer_part start::-->
+<footer class="footer_part" style=" margin-top: 10px; padding: 20px; background-color: #F8F8F8; background-clip: padding;">
+    <div class="container">
+        <div class="row justify-content-around">
+            <div class="col-sm-6 col-lg-3">
+                <div class="single_footer_part">
+                    <h4>Hỗ trợ khách hàng</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Mua hàng trả góp</a></li>
+                        <li><a href="#">Mua hàng online</a></li>
+                        <li><a href="#">Chính sách bảo hành</a></li>
+                        <li><a href="#">Chính sách đổi trả</a></li>
+                        <li><a href="#">Tra cứu bảo hành</a></li>
+                        <li><a href="#">Hình thức thanh toán</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-3">
+                <div class="single_footer_part">
+                    <h4>Về XTmobile</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Tuyển dụng</a></li>
+                        <li><a href="#">Liên hệ hợp tác</a></li>
+                        <li><a href="#">Bán hàng doanh nghiệp</a></li>
+                        <li><a href="#">Cảnh báo giả mạo</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="single_footer_part">
+                    <h4>Tổng đài hỗ trợ</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Tư vấn mua hàng: <br/><span style="color:red">1800.6229</span></a></li>
+                        <li><a href="#">Khiếu nại - Góp ý: <br/><span style="color:red">088.9999933</span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="single_footer_part" style="width:100%">
+                    <h4>Hệ thống cửa hàng</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="https://www.google.com/maps/place/XTmobile+-+CN+Qu%E1%BA%ADn+7/@10.7389566,106.710202,19.52z/data=!4m5!3m4!1s0x31752f92c6fbddb3:0x6c6d1e039a2ac9cc!8m2!3d10.739018!4d106.7101606?hl=vi-VN">396 Nguyễn Thị Thập, Phường Tân Quy, Quận 7, TP.HCM</a></li>
+                        <li><a href="https://www.google.com/maps/place/XTmobile+-+CN+Qu%E1%BA%ADn+1/@10.7927986,106.6941225,20z/data=!4m13!1m7!3m6!1s0x317528cb816fa5f5:0x5c32add2683b521a!2zNTAgVHLhuqduIFF1YW5nIEto4bqjaSwgVMOibiDEkOG7i25oLCBRdeG6rW4gMSwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBWaeG7h3QgTmFt!3b1!8m2!3d10.7925114!4d106.6944131!3m4!1s0x3175294d4c2b295d:0xa6f499cf28763cdd!8m2!3d10.792548!4d106.694373">50 Trần Quang Khải, Phường Tân Định, Quận 1, TP.HCM</a></li>
+                        <li><a href="https://www.google.com/maps/place/XTmobile+-+CN+Qu%E1%BA%ADn+10/@10.7702984,106.673408,20z/data=!4m13!1m7!3m6!1s0x31752edfca5ec8e9:0x180e17370e653c86!2zNjY2IEzDqiBI4buTbmcgUGhvbmcsIFBoxrDhu51uZyAxMCwgUXXhuq1uIDEwLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!3b1!8m2!3d10.7700894!4d106.6735446!3m4!1s0x31752edfcbd39e2f:0xe300556081829529!8m2!3d10.7702089!4d106.6735242">666-668 Lê Hồng Phong, Phường 10, Quận 10, TP.HCM</a></li>
+                        <li><a href="https://www.google.com/maps/place/XTmobile+-+CN+Qu%E1%BA%ADn+9/@10.8481041,106.7753454,21z/data=!4m13!1m7!3m6!1s0x3175270c49ffd30b:0xc41a017318bbb0d9!2zNDMgxJAuIEzDqiBWxINuIFZp4buHdCwgSGnhu4dwIFBow7osIFF14bqtbiA5LCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!3b1!8m2!3d10.8482112!4d106.7754922!3m4!1s0x3175272da6a41ee1:0xbb11bbb233694d28!8m2!3d10.8481163!4d106.7754022?hl=vi-VN">43 Lê Văn Việt, Phường Hiệp Phú, Quận 9, TP.HCM</a></li>
+                        <li><a href="https://www.google.com/maps/place/XTmobile+-+CN+Qu%E1%BA%ADn+G%C3%B2+V%E1%BA%A5p/@10.8331376,106.6658769,19.08z/data=!4m5!3m4!1s0x3175290cf40453df:0x37127195a8b7bba5!8m2!3d10.8333367!4d106.666001?hl=vi-VN">437 Quang Trung, Phường 10, Q.Gò Vấp, TP.HCM</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="copyright_part">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="copyright_text">
+                        <p>
+                            Copyright &copy;
+                            <script>
+                                document.write(new Date().getFullYear());
+                            </script>
+                            All rights reserved by <a href="index.php">XTmobile</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="footer_icon social_icon">
+                        <ul class="list-unstyled">
+                            <li><a href="https://www.facebook.com/XTmobileNo1" target="_blank" class="single_social_icon"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="https://www.instagram.com/xtmobile.official/" target="_blank" class="single_social_icon"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="https://www.youtube.com/@xtmobileNo1" target="_blank" class="single_social_icon"><i class="fab fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--::footer_part end::-->
